9c150f7b29e68e1bc17dc9ac5693ef0e33f7cf10
codeanticode <andres.colubri@gmail.com>
2015-06-17 10:41:21 -0400

    fully working jnius-based script

--- jnius_test.py
+++ jnius_test.py
@@ -1,7 +1,11 @@
-# Test using JNIus, not functional yet.
-
 import sys, codecs, os
-os.environ['CLASSPATH'] = "./*" # Need this to load the classes from the jar files
+
+os.environ['CLASSPATH'] = "../miralib/dist/*" # Need this to load the classes from the jar files
+
+# in pyjnius 1.4+ we can use:
+#import jnius_config
+#jnius_config.add_options('-Xrs', '-Xmx4096')
+#jnius_config.set_classpath('.', '../miralib/dist/*')
 
 from jnius import autoclass
 Log = autoclass("miralib.utils.Log")
@@ -42,6 +46,44 @@ else:
 
 preferences = Preferences()
 project = Project(inputFile, preferences)
-# This hangs the script, bc of calls to PApplet
-# dataset = DataSet(project);
 ranges = DataRanges();
+dataset = DataSet(project);
+
+count = dataset.getVariableCount()
+output = [""] * (count + 1)
+    
+print "Calculating correlation matrix:"
+scores = [[0 for x in xrange(count)] for x in xrange(count)] 
+for i in range(0, count):
+    print "  Row " + str(i) + "/" + str(count) + "..."
+    for j in range(i, count):
+        vi = dataset.getVariable(i)
+        vj = dataset.getVariable(j)
+        slice = dataset.getSlice(vi, vj, ranges)
+        score = 0
+        if i != j and slice.missing < project.missingThreshold():
+            score = Similarity.calculate(slice, project.pvalue(), project)
+        scores[i][j] = scores[j][i] = score        
+print "Done."
+
+header = "";
+for i in range(0, count):
+    vi = dataset.getVariable(i)
+    vname = vi.getAlias().replace('"', '\'');
+    header = header + ";\"" + vname + "\"";
+output[0] = header;
+
+for i in range(0, count):
+    vi = dataset.getVariable(i)
+    vname = vi.getAlias().replace('"', '\'')
+    line = "\"" + vname + "\""
+    for j in range(0, count):
+        line = line + ";" + str(scores[i][j])
+    output[1 + i] = line
+
+file = codecs.open(outputFile, "w", "utf-8")
+for line in output:
+    file.write(line + "\n");
+file.close()    
+
+print "Saved to",outputFile
