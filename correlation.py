ea37fe9260aceca04b251d4562737dbd001f0229
codeanticode <andres.colubri@gmail.com>
2015-07-10 11:33:16 -0400

    use contingency table

--- correlation.py
+++ correlation.py
@@ -1,5 +1,6 @@
 import sys, codecs, os, time
 from scipy.stats import chi2_contingency
+from scipy.stats import fisher_exact
 import numpy as np
 
 os.environ['CLASSPATH'] = "../miralib/dist/*" # Need this to load the classes from the jar files
@@ -35,29 +36,6 @@ Log.init()
 
 # inputFile = "./data_nhanes/config.mira";
 inputFile = "./data_lassa/config.mira";
-outputFile = "./network.csv";
-
-len = len(sys.argv)
-if len == 2:
-    inputFile = sys.argv[1];
-else:
-    for i in range(1, len):    
-        if sys.argv[i] == "-in" and i + 1 < len:
-            inputFile = sys.argv[i + 1];
-        elif sys.argv[i] == "-out" and i + 1 < len:
-            outputFile = sys.argv[i + 1];
-        elif sys.argv[i] == "-miss" and i + 1 < len:
-            preferences.missingString = sys.argv[i + 1];
-        elif sys.argv[i] == "-mist" and i + 1 < len:
-            preferences.missingThreshold = Project.stringToMissing(sys.argv[i + 1]); 
-        elif sys.argv[i] == "-pval" and i + 1 < len:
-            preferences.pValue = Project.stringToPValue(sys.argv[i + 1]);
-        elif sys.argv[i] == "-algo" and i + 1 < len:
-            preferences.depTest = Similarity.stringToAlgorithm(sys.argv[i + 1]);
-        elif sys.argv[i] == "-surr" and i + 1 < len:
-            preferences.surrCount = int(sys.argv[i + 1]);
-        elif sys.argv[i] == "-chtr" and i + 1 < len:
-            preferences.threshold = float(sys.argv[i + 1]);
 
 preferences = Preferences()
 project = Project(inputFile, preferences)
@@ -91,24 +69,30 @@ diff = 0
 for i in range(0, count):
     vari = data.getColumn(i)
     if vari.categorical():
+#     if True:
        print vari.getAlias()
        for j in range(0, count):
            varj = data.getColumn(j)
+#            if True:
            if varj.categorical():
                slice = data.getSlice(vari, varj, ranges)
                if project.missingThreshold() <= slice.missing: continue
                obs = getContingency(slice, project)
                if obs == None: continue
-               chi2, p, dof, ex = chi2_contingency(obs, correction=False)
+               if vari.getCount() == 2 and varj.getCount() == 2:
+                   odds, p = fisher_exact(obs)
+               else:    
+                   chi2, p, dof, ex = chi2_contingency(obs, correction=False)
                score = Similarity.calculate(slice, project.pvalue(), project)
+               m, p2 = PValue.calculate(slice, project)
                chi2_res = p < project.pvalue()
-               simil_res = 0 < score
+               simil_res = p2 < project.pvalue()
                total += 1
                if chi2_res != simil_res: 
                    mark = "<----"
                    diff += 1
                else: mark = ""
-               print vari.getAlias(), varj.getAlias(), p, p < project.pvalue(), 0 < score, mark
+               print vari.getAlias(), varj.getAlias(), p, p2, chi2_res, simil_res, vari.getCount(), varj.getCount(), mark
 t1 = time.time()
 
 print "differences:", diff, "/" , total, "=",(100.0*diff/total), "%"
